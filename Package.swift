93d399ac7ce4e0de1049d1ae3ecf89c32b78fdc7
Dubhe <epoll@foxmail.com>
2024-07-12 10:51:43 +0800

    feat: export CNIOBoringSSL module

--- Package.swift
+++ Package.swift
@@ -59,6 +59,7 @@ let package = Package(
     name: "swift-nio-ssl",
     products: [
         .library(name: "NIOSSL", targets: ["NIOSSL"]),
+        .library(name: "CNIOBoringSSL", type: .static, targets: ["CNIOBoringSSL"]),
         .executable(name: "NIOTLSServer", targets: ["NIOTLSServer"]),
         .executable(name: "NIOSSLHTTP1Client", targets: ["NIOSSLHTTP1Client"]),
 /* This target is used only for symbol mangling. It's added and removed automatically because it emits build warnings. MANGLE_START
